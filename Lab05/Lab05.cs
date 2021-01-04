7852dd2bf340a6810120f47eff23d2f2ce266058
rohithkoritala <56290681+rohithkoritala@users.noreply.github.com>
2021-01-04 11:42:11 +0530

    Add files via upload

---
+++ Lab05/Lab05.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using CPI411.SimpleEngine;
+
+namespace Lab05
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Lab05 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Effect effect;
+        Model model;
+        Texture2D texture;
+
+        Skybox skybox; //Added reference of SimpleEngine
+        Matrix world = Matrix.Identity;
+        Matrix view = Matrix.CreateLookAt(
+                new Vector3(0, 0, 20),
+                Vector3.Zero,
+                Vector3.Up);
+        Matrix projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.ToRadians(90), 800f / 600f, 0.1f, 100f);
+        Vector3 cameraPosition = new Vector3(0, 0, 20);
+        float angle, angle2;
+        float distance = 20;
+        MouseState previousMouseState;
+
+        public Lab05()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            string[] skyboxTextures =
+            {
+                "SunsetPNG1", "SunsetPNG2",
+                "SunsetPNG3", "SunsetPNG4",
+                "SunsetPNG5", "SunsetPNG6",
+            };
+            skybox = new Skybox(skyboxTextures, Content, GraphicsDevice);
+            effect = Content.Load<Effect>("Skybox");
+        }
+    
+    
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                angle -= 0.03f;
+
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                angle += 0.03f;
+
+            }
+            cameraPosition = distance * new Vector3((float)System.Math.Sin(angle), 0, (float)System.Math.Cos(angle));
+            view = Matrix.CreateLookAt(cameraPosition, new Vector3(0, 0, 0), Vector3.UnitY);
+            effect.Parameters["View"].SetValue(view);
+            effect.Parameters["CameraPosition"].SetValue(cameraPosition);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.BlendState = BlendState.Opaque;
+            GraphicsDevice.DepthStencilState = new DepthStencilState();
+
+            RasterizerState ras = new RasterizerState();
+            ras.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = ras;
+
+            skybox.Draw(view, projection, cameraPosition);
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
